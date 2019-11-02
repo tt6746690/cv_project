b7225abc70a1e4c64d04ab906937892eddaead2b
Mark <peiqi1122@gmail.com>
2019-11-02 00:42:27 -0400

    multispectral

---
+++ matlab/run_admm.m
@@ -0,0 +1,37 @@
+function demul_im = run_admm(y,A,InitEstFunc,params,orig_im,varargin)
+    % Run ADMM
+    %
+
+    RatioIntensity = 'intensity';
+    ADMMFunc = @ADMM;
+
+    % Map of parameter names to variable names
+    params_to_variables = containers.Map( ...
+        {'RatioIntensity','ADMMFunc'}, ...
+        {'RatioIntensity','ADMMFunc'});
+    v = 1;
+    while v <= numel(varargin)
+        param_name = varargin{v};
+        if isKey(params_to_variables,param_name)
+            assert(v+1<=numel(varargin));
+            v = v+1;
+            % Trick: use feval on anonymous function to use assignin to this workspace
+            feval(@()assignin('caller',params_to_variables(param_name),varargin{v}));
+        else
+            error('Unsupported parameter: %s',varargin{v});
+        end
+        v=v+1;
+    end
+
+    switch RatioIntensity
+    case 'ratio'
+        input_ratio_im = IntensityToRatio(y);
+        [admm_ratio_im,~,~,~] = ADMMFunc(input_ratio_im,A,InitEstFunc,params,orig_im);
+        demul_im = admm_ratio_im/255;
+        demul_im = RatioToIntensity(demul_im,sum(y,3));
+    case 'intensity'
+        [demul_im,~,~,~] = ADMMFunc(y,A,InitEstFunc,params,orig_im);
+    otherwise
+        warning('not ratio/intensity');
+    end
+end
--- matlab/z_multispectral.m
+++ matlab/z_multispectral.m
@@ -2,13 +2,13 @@
 %
 clc; clear; close all;
 ProjectPaths;
+warning('off', 'MATLAB:MKDIR:DirectoryExists');
 
 %% params 
 
 outdir = 'results/multispectral';
-mkdir(outdir);
 scaling=2;
-light_mode = true;
+light_mode = false;
 [S,F] = deal(5,4);
 [h,w] =  deal(320,324);
 M = SubsamplingMask("tiles",h,w,F,'Tile',[1 2; 3 4]);
@@ -18,63 +18,91 @@ ForwardFunc = @(in_im) reshape(H*in_im(:),h,w,2);
 BackwardFunc = @(in_im) reshape(H'*in_im(:),h,w,S);
 InitEstFunc = InitialEstimateFunc("maxfilter",h,w,F,S,'BucketMultiplexingMatrix',W,'SubsamplingMask',M);
 params_admm = GetDemosaicDemultiplexParams(light_mode);
+perm = [5 2 4 3 1];
+scenes = [
+    "Rubiks_FPS=25.83"
+    "Bunny_FPS=26.54"
+    "Giraffe_FPS=25.61"
+    "Cloud_FPS=22.02"
+];
+
+scene = scenes(1);
+rawimagedir = sprintf('data/MultiSpectral/%s/',scene);
 
 %% 
 
-% scene = 'Bunny_FPS=26.54';
-scene = 'Rubiks_FPS=25.83';
-rawimagedir = sprintf('data/MultiSpectral/%s/',scene);
-shift = 1;
 
-i=292;
+%% initfunc 
+% % 
+% for scene = scenes
+% for i = 1:999
+%     subdir = 'initfunc';
+%     mkdir(sprintf('%s/%s/%s',outdir,scene,subdir));
+%     ims = reconstruct_im(i,h,w,perm,InitEstFunc,scaling,rawimagedir,scene,InitEstFunc);
+%     imwrite(uint8(scaling*ims),sprintf('%s/%s/%s/%.4d.png',outdir,scene,subdir,i-1));
+% end
+% end
+
+%% admmparams
+
 i=490;
 
-ims = [];
-im = [];
+subdir = 'admmparams';
+mkdir(sprintf('%s/%s/%s',outdir,scene,subdir));
+denoiser_types = [
+    "medfilter",
+    "tnrd" 
+];
 
-P = perms(1:S);
+for outer_iters = [5 10 20 40 80]
+params_admm.outer_iters = outer_iters;
 
-% for shift = 0:S-1
-for j = 1:size(P,1)
+for di = [1 2]
+    denoiser_type = denoiser_types(di);
+    params_admm.denoiser_type = denoiser_type;
 
-perm = P(j,:)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-input_im = imread(sprintf('%s/%04d.png',rawimagedir,i-1));
-input_im = double(cat(3,input_im(:,1:w),input_im(:,(w+1):(2*w))));
-% imshow(FlattenChannels(input_im)/255);
+    outer_iters
+    denoiser_type
 
-% %% 1: admm+tnrd in intensity space
-% [admm_intensity_im,~,~,~] = ADMM(input_im,H,InitEstFunc,params_admm,zeros(h,w,S));
+    input_im = imread(sprintf('%s/%04d.png',rawimagedir,i-1));
+    input_im = double(cat(3,input_im(:,1:w),input_im(:,(w+1):(2*w))));
 
+    reconfunc = @(input_im) run_admm(input_im,H,InitEstFunc,params_admm,zeros(h,w,S),'RatioIntensity','intensity','ADMMFunc',@ADMMSmooth);
+    ims = reconstruct_im(i,h,w,perm,InitEstFunc,scaling,rawimagedir,scene,reconfunc);
+    imwrite(uint8(scaling*ims),sprintf('%s/%s/%s/smooth_%s_%d_%.4d.png',outdir,scene,subdir,denoiser_type,outer_iters,i-1));
 
-% imshow(FlattenChannels(admm_intensity_im)/255);
-% imshow(admm_intensity_im(:,:,[3 2 1])/255);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% %% 2. admm+tnrd in ratio space
-% input_ratio_im = IntensityToRatio(input_im);
-% [admm_ratio_im,~,~,~] = ADMM(input_ratio_im,H,InitEstFunc,params_admm,zeros(h,w,S));
-% ratio_mult_inputsum_im = admm_ratio_im/255;
-% ratio_mult_inputsum_im = RatioToIntensity(ratio_mult_inputsum_im,sum(input_im,3));
-
-
-% imshow(FlattenChannels(ratio_mult_inputsum_im));
-% imshow(ratio_mult_inputsum_im(:,:,[3 2 1]));
-
-
-demul_im = InitEstFunc(input_im);
-% demul_im = circshift(demul_im,shift,3);
-% demul_im = demul_im(:,:,[2 5 3 4 1]);
-demul_im = demul_im(:,:,perm);
-
-% imshow(scaling*FlattenChannels(demul_im)/255);
-color_im = demul_im(:,:,[3 2 1]);
-% imshow(scaling*color_im/255);
-
-% im = [im;scaling*FlattenChannels(demul_im)];
-% ims = [ims;scaling*color_im];
-
-imwrite(uint8(scaling*color_im),sprintf('%s/%d-%d-%d-%d-%d.png',outdir,perm(1),perm(2),perm(3),perm(4),perm(5)));
+end
 end
 
-% imshow(im/255);
-% imshow(ims/255);
+%%
+
+
+function y = to_color_im(x)
+    y = x;
+    y(:,:,3) = 0.4*x(:,:,3)+0.4*x(:,:,4); % white balance
+    y = y(:,:,[3 2 1]); % bgr -> rgb
+end
+
+
+function ims = gather_multispectral_ims(im)
+    [h,w,S]=size(im);
+    ims = [im(:,:,1) im(:,:,2) im(:,:,3)
+        im(:,:,4) im(:,:,5) zeros(size(im(:,:,1)))];
+    ims = repmat(ims,[1 1 3]);
+    ims(h+1:end,2*w+1:end,:) = to_color_im(im);
+end
+
+
+function ims = reconstruct_im(i,h,w,perm,InitEstFunc,scaling,rawimagedir,scene,reconfunc)
+    input_im = imread(sprintf('%s/%04d.png',rawimagedir,i-1));
+    input_im = double(cat(3,input_im(:,1:w),input_im(:,(w+1):(2*w))));
+
+    demul_im = reconfunc(input_im);
+    demul_im = demul_im(:,:,perm);
+
+    ims = gather_multispectral_ims(demul_im);
+end
